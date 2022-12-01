a6e26ba50fdebc570b250118a3fbe274b40bfbbe
Pedro Romão <pedro.dias@sou.inteli.com.br>
2022-12-01 16:20:54 -0300

    front integrado

---
+++ src/frontend/js/index.js
@@ -0,0 +1,20 @@
+setInterval(function(){
+    var url = "http://10.128.65.251:3031/retornoTotem";
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();//The script's execution stops here until the server returns the requirements 
+    var response = JSON.parse(xhttp.responseText);
+
+    console.log(xhttp.responseText)
+
+    document.getElementById('table').innerHTML = `<tr>
+    <th>Posição</th>
+    <th>Placa</th>
+    <th>Tempo</th>
+  </tr>`
+
+    for(var i = 0; i<Object.keys(response).length; i++){
+        document.getElementById('table').innerHTML += '<tr><td><p>' + (i + 1) + '</p></td><td><p>'+ response[i].placa + '</p></td><td><p>' + response[i].tempoEstimado + '</p></td></tr>'
+    }
+
+},  1000);
